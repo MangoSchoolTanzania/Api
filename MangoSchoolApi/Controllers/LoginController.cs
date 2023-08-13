3c9e1ac10ad99f8faf9a24f849cd77847cacd61c
MateusMo <mat.marq.oliveira@gmail.com>
2023-08-13 17:19:37 -0300

    Feature: Login Controller

---
+++ MangoSchoolApi/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using MangoSchoolApi.Data;
+using MangoSchoolApi.Models;
+using MangoSchoolApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangoSchoolApi.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/v1/login")]
+        public IActionResult Login([FromServices] MangoDataContext context, [FromBody]LoginViewModel credentials)
+        {
+            try
+            {
+                if(credentials == null) { return StatusCode(400, " The credentials provided are not valid"); }
+
+                var userContext = context.Users.Where(x => x.UserEmail == credentials.Email && x.Password == credentials.Password).FirstOrDefault();
+
+                if (userContext == null) { return StatusCode(400, " No user was found"); };
+
+                var user = new User();
+                user = userContext;
+
+                return Ok(user);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

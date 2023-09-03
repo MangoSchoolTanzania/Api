db780d4ead566b592df056d0fb01be44137f2e64
MateusMo <mat.marq.oliveira@gmail.com>
2023-09-03 17:56:32 -0300

    JWT implementation

--- MangoSchoolApi/Controllers/LoginController.cs
+++ MangoSchoolApi/Controllers/LoginController.cs
@@ -1,34 +1,78 @@
 ﻿using MangoSchoolApi.Data;
-using MangoSchoolApi.Models;
 using MangoSchoolApi.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MangoSchoolApi.Controllers
 {
+    [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost]
         [Route("/v1/login")]
-        public IActionResult Login([FromServices] MangoDataContext context, [FromBody]LoginViewModel credentials)
+        public async Task<IActionResult> Login([FromServices] MangoDataContext context, [FromBody]LoginViewModel credentials)
         {
             try
             {
                 if(credentials == null) { return StatusCode(400, " The credentials provided are not valid"); }
 
-                var userContext = context.Users.Where(x => x.UserEmail == credentials.Email && x.Password == credentials.Password).FirstOrDefault();
+                var isValidUser = await IsValidUser(context, credentials.Email, credentials.Password);
 
-                if (userContext == null) { return StatusCode(400, " No user was found"); };
+                if (isValidUser)
+                {
+                    var token = GenerateJwtToken(credentials.Email);
+                    return Ok(new { token });
+                }
 
-                var user = new User();
-                user = userContext;
-
-                return Ok(user);
+                return Unauthorized();
             }
             catch (Exception)
             {
 
                 throw;
             }
+        }
+
+        private async Task<bool> IsValidUser(MangoDataContext Context, string email, string password)
+        {
+            var user = await Context.Users.FirstOrDefaultAsync(x => x.UserEmail == email && x.Password == password);
+            if (user != null) { return true; }
+
+            return false;
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(JwtRegisteredClaimNames.Aud, _configuration["JwtSettings:Audience"]),
+                    new Claim(JwtRegisteredClaimNames.Iss, _configuration["JwtSettings:Issuer"])
+                 }),
+                Expires = DateTime.UtcNow.AddHours(8), // Set token expiration time
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
--- MangoSchoolApi/Program.cs
+++ MangoSchoolApi/Program.cs
@@ -1,3 +1,9 @@
+using MangoSchoolApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MangoDataContext>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+    };
+});
 
 var app = builder.Build();
 
